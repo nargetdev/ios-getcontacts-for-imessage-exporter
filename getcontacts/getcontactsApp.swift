db1839e55b2b47c0eb98c0696821161833080a1e
Nathan Argetsinger <nathan@lynx.eco>
2024-02-19 12:39:39 -0800

    Initial Commit

---
+++ getcontacts/getcontactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  getcontactsApp.swift
+//  getcontacts
+//
+//  Created by Ms Whiskers on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct getcontactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
