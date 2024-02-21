0c5c39ef3fea30f126eabb9ffaac40058dd8b163
Nathan Argetsinger <nathan@lynx.eco>
2024-02-20 20:24:50 -0800

    print JSON or CSV contacts

--- getcontacts/ContentView.swift
+++ getcontacts/ContentView.swift
@@ -13,7 +13,12 @@ func fetchAndMatchContacts() async -> [String] {
     let store = CNContactStore()
     var results: [String] = []
     do {
-        print("[")
+        let JSON_not_CSV = false
+        if JSON_not_CSV{
+            print("[")
+        } else {
+            print("number, name")
+        }
         let granted = try await store.requestAccess(for: .contacts)
         guard granted else {
             print("Access to contacts was denied.")
@@ -27,11 +32,17 @@ func fetchAndMatchContacts() async -> [String] {
                 let number = phoneNumber.value.stringValue
                 // Implement your matching logic here
                 // If matched, append to results
-                print("{\"number\":" + " \"\(number)\", \"name\": \"\(contact.familyName) \(contact.givenName)\"},")
+                if (JSON_not_CSV){
+                    print("{\"number\":" + " \"\(number)\", \"name\": \"\(contact.familyName) \(contact.givenName)\"},")
+                }
+                else {
+                    print("\(number), \(contact.familyName) \(contact.givenName)")
+                }
             }
         }
-        
-        print("]")
+        if (JSON_not_CSV){
+            print("]")
+        }
         return results
     } catch {
         print("Failed to fetch contacts: \(error)")
